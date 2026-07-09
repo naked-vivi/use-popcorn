f80c7f4a2fa930180fb0aff5c8f3e2b2e49d44ea
Nang Pyae Pyae Phyoe Aein <pyaephyoeaein@gmail.com>
2026-07-10 01:47:54 +0700

    refactor: compose main panels with shared box

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import MainPage from "./components/layout/MainPage";
 import NavBar from "./components/layout/NavBar";
 import { tempMovieData } from "./data/movies";
-import type { Movie } from "./types";
+import type { Movie, WatchedMovie } from "./types";
 import NumResult from "./components/shared/NumResult";
 import Search from "./components/shared/Search";
+import Box from "./components/shared/Box";
+import MovieList from "./components/movies/MovieList";
+import WatchedList from "./components/watched/WatchedList";
+import WatchedSummary from "./components/watched/WatchedSummary";
+import { tempWatchedData } from "./data/watched";
+import StarRating from "./components/shared/StarRating";
 
 export default function App() {
   const [movies] = useState<Movie[]>(tempMovieData);
+  const [watched] = useState<WatchedMovie[]>(tempWatchedData);
 
   return (
     <>
@@ -15,7 +22,19 @@ export default function App() {
         <Search />
         <NumResult movies={movies} />
       </NavBar>
-      <MainPage movies={movies} />
+
+      <StarRating maxRating={5} />
+
+      <MainPage>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedList watched={watched} />
+        </Box>
+      </MainPage>
     </>
   );
 }
--- src/components/layout/MainPage.tsx
+++ src/components/layout/MainPage.tsx
@@ -1,16 +1,13 @@
-import MovieListBox from "../movies/MovieListBox";
-import WatchedBox from "../watched/WatchedBox";
-import type { Movie } from "../../types";
+
 
 interface MainPageProps {
-    movies: Movie[];
+    children?: React.ReactNode;
 }
 
-function MainPage({ movies }: MainPageProps) {
+function MainPage({ children }: MainPageProps) {
     return (
         <main className="main">
-            <MovieListBox movies={movies} />
-            <WatchedBox />
+            {children}
         </main>
     )
 }
